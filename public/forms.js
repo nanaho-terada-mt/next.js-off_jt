cf91bd25f8d5b3760f434e682fd1d682220b5300
MT 寺田 菜々穂 <nanaho.terada@metateam.co.jp>
2025-05-21 10:59:51 +0900

    Revert "改行とJSの構文修正行いました"
    This reverts commit b3a292ac12435aaa05aaedf9cbf29f6b0fd39fc5.

--- public/forms.js
+++ public/forms.js
@@ -8,38 +8,33 @@ form.addEventListener("submit", (e) => {
 	const profession = document.getElementById("profession").value;
 	const introduce = document.getElementById("introduce").value.trim();
 
-	try {
-		let hasError = false;
+	let hasError = false;
 
-		if (username === "") {
-			document.getElementById("nameError").style.display = "block";
-			hasError = true;
-		} else {
-			document.getElementById("nameError").style.display = "none";
-		}
-		if (age === "" || age < 18) {
-			document.getElementById("ageError").style.display = "block";
-			hasError = true;
-		} else {
-			document.getElementById("ageError").style.display = "none";
-		}
-
-		if (hasError) {
-			throw new Error("エラー発生！");
-		}
-
-		const data = {
-			name: username,
-			age: age,
-			profession: profession,
-			introduce: introduce,
-		};
-
-		const jsonData = JSON.stringify(data, null, 2);
-
-		console.log(jsonData);
-		alert("送信完了！");
-	} catch (error) {
-		console.error("バリデーションチェック中…", error.message);
+	if (username === "") {
+		document.getElementById("nameError").style.display = "block";
+		hasError = true;
+	} else {
+		document.getElementById("nameError").style.display = "none";
 	}
+	if (age === "" || age < 18) {
+		document.getElementById("ageError").style.display = "block";
+		hasError = true;
+	} else {
+		document.getElementById("ageError").style.display = "none";
+	}
+
+	if (hasError) {
+		return;
+	}
+
+	const data = {
+		name: username,
+		age: age,
+		profession: profession,
+		introduce: introduce,
+	};
+
+	const jsonData = JSON.stringify(data, null, 2);
+
+	console.log(jsonData);
 });
