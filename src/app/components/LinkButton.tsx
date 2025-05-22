0d021c5256d27b6f204e74c60650500d4278dab9
MT 寺田 菜々穂 <nanaho.terada@metateam.co.jp>
2025-05-22 14:22:43 +0900

    関数コンポーネントの演習完了しました

---
+++ src/app/components/LinkButton.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useState } from "react";
+
+export function LinkButton() {
+	const [count, setCount] = useState(0);
+
+	return (
+		<button type="button" onClick={() => setCount(count + 1)}>
+			❤️ {count}
+		</button>
+	);
+}
